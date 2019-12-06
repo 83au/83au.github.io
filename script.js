60cbc166d5b2dec90c7c4f8eb18e99838b059e19
Beau Sherman <brsherman88@gmail.com>
2019-12-06 12:21:04 -0800

    Adjust styles

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
   // Set copyright date
-  $('footer').append((new Date()).getFullYear());
+  $('footer').append(new Date().getFullYear());
 
   // Smooth scroll for safari and ios browsers
   $('[href^="#"]').smoothScroll();
