0a48561a899ad88792349d8012591eb497357078
Beau Sherman <brsherman88@gmail.com>
2020-06-14 14:16:16 -0700

    Add minicssExtractPlugin and sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -9,11 +10,20 @@ module.exports = {
   },
 
   plugins: [
-    new HtmlWebpackPlugin({template: './src/index.html'})
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new MiniCssExtractPlugin()
   ],
 
   module: {
     rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
       {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
